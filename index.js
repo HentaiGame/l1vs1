f1879c411d50692ea5b5b83cee91668da4857f10
HentaiGame <100564706+HentaiGame@users.noreply.github.com>
2022-03-31 19:28:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+{
+  "name": "quickstart",
+  "version": "1.0.0",
+  "description": "Azure Cognitive Search Quickstart",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "Azure",
+    "Search"
+  ],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "@azure/search-documents": "^11.2.0",
+    "dotenv": "^8.2.0"
+  }
+}
